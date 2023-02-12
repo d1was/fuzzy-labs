82091bc6cc05317c8c997a7c6cc90299f1c6189e
Diwas KC <diwaskc12@gmail.com>
2023-02-12 18:32:25 +0545

    subset complete

--- lab-1/index.js
+++ lab-1/index.js
@@ -1,57 +1,66 @@
 // Crisp Set Input
-const superset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const setA = [1, 2, 3];
 const setB = [5, 2, 1];
 
-const mappedA = mapping(superset, setA);
-const mappedB = mapping(superset, setB);
+const mappingForX = mapping(X);
+const mappedA = mappingForX(setA);
+const mappedB = mappingForX(setB);
 
 console.log(`Mapped of set A = ${mappedA.toString()}`)
-console.log(`Mapped of set B = ${mappedA.toString()}`)
+console.log(`Mapped of set B = ${mappedB.toString()}`)
 
-const unionResult = union(mappedA, mappedB);
+const unionResult = set(mappedA).union(mappedB);
 console.log(`Result of union of A and B = ${unionResult.toString()}`);
 
-const intersectionResult = intersection(mappedA, mappedB);
+const intersectionResult = set(mappedA).intersection(mappedB);
 console.log(`Result of intersection of A and B = ${intersectionResult.toString()}`);
 
-const complementResult = complement(mappedA);
+const complementResult = set(mappedA).complement();
 console.log(`Result of complement of A = ${complementResult.toString()}`);
 
+const isBsubsetOfA = set(mappedB).isSubsetOf(mappedA);
+console.log(`B is subset of A = ${isBsubsetOfA}`);
+
+
 // Mapping Function
-function mapping(superset, set) {
-    if (!setA.every((item) => superset.includes(item))) {
-        throw new Error("Given set is not the subset of given superset.")
+function mapping(superset) {
+    return function(set) {
+        return superset.map(function (item) {
+            return set.includes(item) ? 1 : 0;
+        });
     }
-    return superset.map(function (item) {
-        return set.includes(item) ? 1 : 0;
-    });
 }
 
-// Union of two set
-function union(setA, setB) {
-    if (setA.length !== setB.length) {
-        throw new Error("provided sets are not equal")
-    }
-    return setA.map(function (item, index) {
-        return Math.max(item, setB[index]);
-    })
-}
 
-// Intersection of two set
-function intersection(setA, setB) {
-    if (setA.length !== setB.length) {
-        throw new Error("provided set are not equal");
-    }
-    return setA.map(function (item, index) {
-        return Math.min(item, setB[index]);
-    })
-}
 
-// Complement of two set
-function complement(setA) {
-    return setA.map(function (item) {
-        return 1 - item;
-    });
+// Set operations
+function set(firstSet) {
+    return {
+        union(secondSet) {
+            if (firstSet.length !== secondSet.length) {
+                throw new Error("provided sets are not equal")
+            }
+            return firstSet.map(function (item, index) {
+                return Math.max(item, secondSet[index]);
+            })
+        },
+        intersection(secondSet) {
+            if (firstSet.length !== secondSet.length) {
+                throw new Error("provided set are not equal");
+            }
+            return firstSet.map(function (item, index) {
+                return Math.min(item, secondSet[index]);
+            })
+        },
+        complement() {
+            return firstSet.map(function (item) {
+                return 1 - item;
+            });
+        },
+        isSubsetOf(secondSet) {
+            return firstSet.every((item, index) => item <= secondSet[index]);
+        }
+    }
 }
